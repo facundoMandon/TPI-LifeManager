31f81bbe5b8a0089dd5de0a2da0b7217b5c28151
facundoMandon <facundo-mandon@hotmail.com>
2025-04-08 11:02:02 -0300

    Corrijo errores en los export e import, las carpetas estaban mal colocadas y ademas, incluí un index.js en las carpetas ./components y ./sections para poder exportar los componentes de manera mas sencilla, en bloque.

---
+++ TPI-LifeManager/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+        <ul>
+          <li><a href="#ejercicio">Ejercicio</a></li>
+          <li><a href="#estudios">Estudios</a></li>
+          <li><a href="#hobbies">Hobbies</a></li>
+          <li><a href="#trabajo">Trabajo</a></li>
+        </ul>
+      </nav>
+    )
+}
+
+export default NavBar;
